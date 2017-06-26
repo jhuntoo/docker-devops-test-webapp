b7b4fc4e3b69ec2a3bd9366d5d5c0b709d50a5ac
jhuntoo <jonnolee84@gmai.com>
2017-06-26 11:37:07 +0100

    use sudo mv --force

--- scripts/install_docker.sh
+++ scripts/install_docker.sh
@@ -2,4 +2,4 @@ set -x
 VER="17.03.0-ce"
 curl -L -o /tmp/docker-$VER.tgz https://get.docker.com/builds/Linux/x86_64/docker-$VER.tgz
 tar -xz -C /tmp -f /tmp/docker-$VER.tgz
-mv --force /tmp/docker/* /usr/bin
+sudo mv --force /tmp/docker/* /usr/bin
